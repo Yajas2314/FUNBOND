53d6882ceedac0a169d415991f1fc614e0a1c783
Yajas2314 <yajasagrawal@gmail.com>
2026-06-12 22:19:47 +0530

    Update FUNBOND.jsx

--- FUNBOND.jsx
+++ FUNBOND.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 import img1 from './Image1.jpg';
 import img2 from './Image2.jpg';
 import img3 from './Image3.jpg';
-import img4 from './Image4.PNG';
-import img5 from './Image5.PNG';
+import img4 from './Image4.png';
+import img5 from './Image5.png';
 
 const Navigation = () => {
   return (
