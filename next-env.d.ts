7e28bb833262fae10cfd132dbd4f01610e508a48
Yajas2314 <yajasagrawal@gmail.com>
2026-06-12 22:15:53 +0530

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+declare module '*.jpg' {
+  const content: any;
+  export default content;
+}
+declare module '*.jpeg' {
+  const content: any;
+  export default content;
+}
+declare module '*.PNG' {
+  const content: any;
+  export default content;
+}
