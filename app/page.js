cc7fa8684a528868b61953b1699148cbc668c0dd
Yajas2314 <yajasagrawal@gmail.com>
2026-06-13 11:12:49 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import img1 from '../gallery/image1.jpg';
-import img2 from '../gallery/image2.jpg';
-import img3 from '../gallery/image3.jpg';
-import img4 from '../gallery/image4.png';
-import img5 from '../gallery/image5.png';
+import img1 from '.Image1.jpg';
+import img2 from '.Image2.jpg';
+import img3 from '.Image3.jpg';
+import img4 from '.Image4.png';
+import img5 from '.Image5.png';
