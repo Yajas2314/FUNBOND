0dc0abf5ebc0df9531132d979e3b5a0a5554b437
Yajas2314 <yajasagrawal@gmail.com>
2026-06-13 11:06:27 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
-'use client';
-
 import React from 'react';
-import FUNBOND from './FUNBOND'; // Changed from '../FUNBOND'
 
-export default function Page() {
-  return <FUNBOND />;
-}
+import img1 from '../gallery/image1.jpg';
+import img2 from '../gallery/image2.jpg';
+import img3 from '../gallery/image3.jpg';
+import img4 from '../gallery/image4.png';
+import img5 from '../gallery/image5.png';
