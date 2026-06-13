9e195cd7d0e7bf85e0e4ca21e2156fc0bfbf293c
Yajas2314 <yajasagrawal@gmail.com>
2026-06-13 10:28:09 +0530

    Refactor FUNBOND component and image paths
    Updated image imports and adjusted component structure.

--- FUNBOND.jsx
+++ FUNBOND.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 // Relative imports from the exact same directory as this file
-import img1 from './Gallery/Image1.jpg';
-import img2 from './Gallery/Image2.jpg';
-import img3 from './Gallery/Image3.jpg';
-import img4 from './Gallery/Image4.PNG';
-import img5 from './Gallery/Image5.PNG';
+import img1 from './Image1.jpg';
+import img2 from './Image2.jpg';
+import img3 from './Image3.jpg';
+import img4 from './Image4.png';
+import img5 from './Image5.png';
 
 const Navigation = () => {
   return (
