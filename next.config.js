b75a586acb2a0dd59fe882e59b347b1021b25f74
Yajas2314 <yajasagrawal@gmail.com>
2026-06-13 11:00:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').Config} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // 1. Completely disable the default vendor split chunk behavior
-      config.optimization.splitChunks = {
-        cacheGroups: {
-          default: false,
-          vendors: false,
-        },
-      };
-      
-      // 2. Safe Webpack 5 compliant unique naming for JS chunks
-      config.output.chunkFilename = 'static/chunks/[name].[contenthash:8].js';
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
